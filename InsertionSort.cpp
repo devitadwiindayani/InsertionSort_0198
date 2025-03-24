c56d17d6f87ffa2e6eac8e154ba3031dcc2ae219
devitadwiindayani <devitadwiindayani@gmail.com>
2025-03-24 16:03:29 +0700

    Menambahkan proses pengisian element array pada fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -16,5 +16,18 @@ void input()
         break;
         }
         else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
+        }
     }
+cout << endl; // Membuat jarak per baris program
+cout << "======================" << endl; // membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
+
+for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+{
+    cout << "Data ke-" << (i + 1) << "; "; // Memasukan atau menginputkan nilai data n
+    cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+}
 }
