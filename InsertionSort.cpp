b2d9b833467302281f8a73b00881cc187186269c
devitadwiindayani <devitadwiindayani@gmail.com>
2025-03-24 15:53:00 +0700

    Menambahkan validasi jumlah data pada fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,18 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
+
+void input()
+{ // Procedure Input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlahh element Array
+        cin >> n; // memanggil variable inputan n
+
+        if (n <= 20)
+        { // Membuat Kondisi n tidak lebih dari 20
+        break;
+        }
+        else
+    }
+}
