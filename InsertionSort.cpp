a7ef48617d8027fc9df2404a01851e64dba3ba97
devitadwiindayani <devitadwiindayani@gmail.com>
2025-03-24 16:14:04 +0700

    Mengimplementasikan proses pengurutan

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -39,4 +39,18 @@ void insertionSort()
     int j, i; // Membuat variable j sebagai penanda
     
     for (i = 1; i <= n -1; i++)
+    { // Step 1
+
+        temp = arr[i]; // Step 2
+
+        j = i - 1; // Step 3
+
+        while (j >= 0 && arr[j] > temp) // Step 4 
+        {
+            arr[j + 1] = arr[j]; // Step 4a
+            j--; // Step 4b
+        }
+
+        arr[j + 1] = temp; // Step 5
+    }
 }
